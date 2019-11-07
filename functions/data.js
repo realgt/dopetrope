6d22e42543d94fc32405d4e30be7c3879bb80ba3
Fabio Rodriguez <realgt@gmail.com>
2019-11-06 22:40:30 -0500

    moved function

--- functions/data.js
+++ functions/data.js
@@ -1,9 +1,21 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ',
-      event: event,
-    })
+  try{
+    // const latitude  = event.queryStringParameters.latitude;
+    // const longitude = event.queryStringParameters.longitude;
+
+    const response = await fetch('https://jsonbox.io/box_10807316f9b18c89c4cb');
+    const data     = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.toString()
+    };
   }
-}
+};
